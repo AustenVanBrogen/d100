295d680c2ed3bfdb815a3a225f32ba25497570b8
AustenVanBrogen <austenvanbrogen@yahoo.com>
2022-03-16 23:30:50 -0500

    Started work on the bot recognizing commands

--- d100.js
+++ d100.js
@@ -3,5 +3,23 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 require("dotenv").config();
 
-const token = process.env.BOT_TOKEN;
+const botToken = process.env.BOT_TOKEN;
 const prefix = '!';
+
+client.once('ready', () => {
+    console.log("d100 bot is online");
+});
+
+client.on('messageCreate', async(message) =>{
+    if((message.author.bot) || (!message.content.startsWith(prefix))) return;
+
+    const args = message.content.slice(prefix.length).split("/ +/");
+    const command = args.shift().toLowerCase();
+
+    if (command === 'roll')
+    {
+        message.channel.send("That was a roll");
+    }
+});
+
+client.login(botToken);
