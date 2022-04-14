a7226327931f8a5b9cd407bbb894b99e459b42a2
AustenVanBrogen <austenvanbrogen@yahoo.com>
2022-04-14 16:17:36 -0500

    Removed youtube-dl

--- d100.js
+++ d100.js
@@ -1,7 +1,11 @@
 const { channel } = require('diagnostics_channel');
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, 
+                                        Intents.FLAGS.GUILD_MESSAGES, 
+                                        Intents.FLAGS.GUILD_VOICE_STATES] });
 require("dotenv").config();
+const { DisTube } = require('distube');
+const distube = new DisTube(client, {emitNewSongOnly: true, searchSongs: 0} );
 
 const botToken = process.env.BOT_TOKEN;
 const prefix = '!';
@@ -16,17 +20,44 @@ client.on('messageCreate', async(message) =>{
     const args = message.content.slice(prefix.length).split("/ +/");
     const command = args.shift().toLowerCase();
 
+
+    const status = queue =>
+  `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.join(', ') || 'Off'}\` | Loop: \`${
+    queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
+  }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
+client.distube
+  .on('playSong', (queue, song) =>
+    queue.textChannel.send(
+      `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${
+        song.user
+      }\n${status(queue)}`
+    )
+  )
+  .on('addSong', (queue, song) =>
+    queue.textChannel.send(
+      `${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+    )
+  )
+  .on('addList', (queue, playlist) =>
+    queue.textChannel.send(
+      `${client.emotes.success} | Added \`${playlist.name}\` playlist (${
+        playlist.songs.length
+      } songs) to queue\n${status(queue)}`
+    )
+  )
+  .on('error', (channel, e) => {
+    channel.send(`${client.emotes.error} | An error encountered: ${e.toString().slice(0, 1974)}`)
+    console.error(e)
+  })
+  .on('empty', channel => channel.send('Voice channel is empty! Leaving the channel...'))
+  .on('searchNoResult', (message, query) =>
+    message.channel.send(`${client.emotes.error} | No result found for \`${query}\`!`)
+  )
+  .on('finish', queue => queue.textChannel.send('Finished!'));
+
     if (command === 'roll')
     {
         //message.channel.send("That was a roll");
-        let channel = message.author.channel;
-        channel.join().then(connection => {
-            // Yay, it worked!
-            console.log("Successfully connected.");
-          }).catch(e => {
-            // Oh no, it errored! Let's log it to console :)
-            console.error(e);
-          });
     }
 });
 
